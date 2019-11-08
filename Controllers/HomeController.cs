08c808a435e602899193ce8e0622fc745f2f6467
John Weisz <johnmweisz@gmail.com>
2019-11-08 12:05:42 -0800

    fixed reply email error

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,23 +31,24 @@ namespace Portfolio.Controllers
 
                     MailboxAddress from = new MailboxAddress($"{contactform.Name}", $"{contactform.Email}");
                     message.From.Add(from);
+                    message.ReplyTo.Add(from);
 
-                    MailboxAddress to = new MailboxAddress("John Weisz", "johnmweisz@gmail.com");
+                    MailboxAddress to = new MailboxAddress("John Weisz", Configuration["EmailInfo:Email"]);
                     message.To.Add(to);
 
                     if(contactform.Phone == null)
                     {
                         contactform.Phone = "No number included.";
                     }
-                    message.Subject = $"New Contact from {contactform.Phone}";
+                    message.Subject = $"{contactform.Name} Wants to Connect!";
                     
                     if(contactform.Message == null)
                     {
                         contactform.Message = "No message included.";
                     }
                     BodyBuilder bodyBuilder = new BodyBuilder();
-                    bodyBuilder.HtmlBody = $"<p>{contactform.Message}</p>";
-                    bodyBuilder.TextBody = $"{contactform.Message}";
+                    bodyBuilder.HtmlBody = $"<p>{contactform.Message}</p><p>{contactform.Email}</p><p>{contactform.Phone}</p>";
+                    bodyBuilder.TextBody = $"{contactform.Message} \n{contactform.Email} \n{contactform.Phone}";
                     message.Body = bodyBuilder.ToMessageBody();
 
                     message.Body = bodyBuilder.ToMessageBody();
